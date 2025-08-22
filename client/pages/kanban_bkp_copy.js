1fb322a6da26dd622662b757e3ee75b43b5f1d0f
Tejas Gumgaonkar <tejassgg@gmail.com>
2025-08-21 20:03:28 -0700

    Kanban Bkp Deleted

--- client/pages/kanban_bkp_copy.js
+++
@@ -1,4 +0,0 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import Head from 'next/head';
-import { useAuth } from '../context/AuthContext';
-import { useTheme } from '../context/ThemeContext';
